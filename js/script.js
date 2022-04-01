50f6c8219e56be1d011ecdc7206c1c5195afb725
JonkiJu <87801872+JonkiJu@users.noreply.github.com>
2022-04-01 18:05:11 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function burgerMenu(selector) {
+  let menu = $(selector);
+  let button = menu.find('.burger-menu_button', '.burger-menu_lines');
+  let links = menu.find('.burger-menu_link');
+  let overlay = menu.find('.burger-menu_overlay');
+  
+  button.on('click', (e) => {
+    e.preventDefault();
+    toggleMenu();
+  });
+  
+  links.on('click', () => toggleMenu());
+  overlay.on('click', () => toggleMenu());
+  
+  function toggleMenu(){
+    menu.toggleClass('burger-menu_active');
+    
+    if (menu.hasClass('burger-menu_active')) {
+      $('body').css('overlow', 'hidden');
+    } else {
+      $('body').css('overlow', 'visible');
+    }
+  }
+}
+
+burgerMenu('.burger-menu');
